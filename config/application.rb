8f3e7622d75ce57461ba8c3c0361889224ffe9f2
Juan C. Garcia <juancgi@gmail.com>
2012-11-13 13:49:23 -0600

    fix stuff

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require "active_model/railtie"
-require "action_controller/railtie"
-# require "action_mailer/railtie"
-# require "active_resource/railtie"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
-
+require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  #Bundler.require(*Rails.groups(:assets => %w(development test)))
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  Bundler.require(:default, :assets, Rails.env)
+  # Bundler.require(:default, :assets, Rails.env)
 end
 
 module JuanCristobalMusic
@@ -60,6 +54,9 @@ module JuanCristobalMusic
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
   end
 end
