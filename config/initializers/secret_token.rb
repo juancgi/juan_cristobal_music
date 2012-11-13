fccf0a10c353f6c006128abddbf33a7d64869048
Juan C. Garcia <juancgi@gmail.com>
2012-11-12 23:40:59 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JuanCristobalMusic::Application.config.secret_token = '227f0e24cd6206aa61a684739651cc57bc565c3cd062d881e8f3390aa92f324209dcdc07339a86eac4b537a4ca7700a57e1d51e51223f2252dc30670b3dd078f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+JuanCristobalMusic::Application.config.session_store :cookie_store, key: '_juan_cristobal_music_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# JuanCristobalMusic::Application.config.session_store :active_record_store
